c8264e6033a9e4f4bba8fe7d6a7cf3308d1d039f
Daniel J Ley <danieljley@Admins-MacBook-Pro.local>
2017-01-13 15:52:16 -0500

    Add forms, actions, and initial state

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -2,11 +2,12 @@ import * as types from '../actions/actionTypes';
 
 export default function courseReducer(state = [], action) {
   switch(action.type) {
-    case types.CREATE_COURSE:
-      return [...state,
-        Object.assign({}, action.course)
-      ]; // spread operator. Spreads the array. Returns new instance of our array.
-    
+    case types.LOAD_COURSES_SUCCESS:
+      // return [...state,
+      //   Object.assign({}, action.course)
+      // ]; // spread operator. Spreads the array. Returns new instance of our array.
+      return action.courses;
+      
     default:
       return state;
   }
