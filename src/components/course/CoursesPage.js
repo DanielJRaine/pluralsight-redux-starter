54637fde6a767ee85aa64c72308a492cd15733cc
Daniel J Ley <danieljley@Admins-MacBook-Pro.local>
2017-01-13 14:38:46 -0500

    Separate coursepage concerns to new file

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -5,26 +5,8 @@ import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
-      super(props, context);
-
-      this.state = {
-        course: { title: "" }
-      };
-
+      super(props, context); 
       // place bind calls in constructor
-      this.onTitleChange = this.onTitleChange.bind(this);
-      this.onClickSave = this.onClickSave.bind(this);
-  }
-
-// Child functions
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({course: course});
-  }
-
-  onClickSave() {
-    this.props.actions.createCourse(this.state.course);
   }
 
   courseRow(course, index) {
@@ -37,16 +19,6 @@ class CoursesPage extends React.Component {
       <div>
         <h1>Courses</h1>
         {this.props.courses.map(this.courseRow)}
-        <h2>Add Course</h2>
-        <input 
-          type="text"
-          onChange={this.onTitleChange}
-          value={this.state.course.title} />
-
-        <input 
-          type="submit"
-          value="Save"
-          onClick={this.onClickSave} />
       </div>
     );
   }
