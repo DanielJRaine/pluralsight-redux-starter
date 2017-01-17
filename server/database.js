4d2ebf09f2ce735ca0ef7279e208393ffff32baf
Daniel J Ley <danieljley@Admins-MacBook-Pro.local>
2017-01-17 15:31:47 -0500

    Edit details of schema to match Mochawesome report

--- server/database.js
+++ server/database.js
@@ -1,7 +1,14 @@
 'use strict';
 
 let mongoose = require('mongoose');
+let Report = require('./models/Report.js');
 
 mongoose.connect('mongodb://localhost/test', () => {
   console.log("connected");
-})
+
+  mongoose.connection.db.dropDatabase();
+
+  reports.forEach((report) => {
+    new Report(report).save();
+  });
+});
