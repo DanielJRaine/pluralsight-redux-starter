79ffabd45d01a0ec674c7a64fc104c3112f1b5aa
Daniel J Ley <danieljley@Admins-MacBook-Pro.local>
2017-01-17 10:33:43 -0500

    Build TimelapsePage component

--- src/components/timelapse/TimelapsePage.js
+++ src/components/timelapse/TimelapsePage.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import {Link} from 'react-router';
 import CalendarHeatmap from 'react-calendar-heatmap';
 
@@ -8,21 +10,37 @@ class TimelapsePage extends React.Component {
 
   }
 
-
+  render() {
+    return (
+      <CalendarHeatmap
+        endDate={new Date('2016-04-01')}
+        numDays={100}
+        values={[
+          { date: '2016-01-01' },
+          { date: '2016-01-22' },
+          { date: '2016-01-30' }
+        ]}
+        classForValue={(value) => {
+          if (!value) {
+            return 'color-empty';
+          }
+          return `color-scale-${value.count}`;
+        }}
+      />
+    )
+  }
 }
 
-<CalendarHeatmap
-  endDate={new Date('2016-04-01')}
-  numDays={100}
-  values={[
-    { date: '2016-01-01' },
-    { date: '2016-01-22' },
-    { date: '2016-01-30' }
-  ]}
-   classForValue={(value) => {
-    if (!value) {
-      return 'color-empty';
-    }
-    return `color-scale-${value.count}`;
-  }}
-  />
+const mapStateToProps = (state, ownProps) => {
+  return {
+    prop: state.prop
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    actions: bindActionCreators(timelapseActions, dispatch)
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TimelapsePage);
