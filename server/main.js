64a6c2a94bbc4bc954e3d0db6908ec44f04d772a
Daniel J Ley <danieljley@Admins-MacBook-Pro.local>
2017-01-17 13:36:46 -0500

    Destructure filetree to resolve webpack path issue

--- server/main.js
+++ server/main.js
@@ -5,6 +5,9 @@ const express = require('express');
 let app = new express();
 
 app.get('/', (req, res) => {
-  res.render('./../app/index.html',{});
+  res.render('./../src/index.html',{});
 })
+.use(express.static(__dirname + '/../.tmp'))
 .listen(7777);
+
+require('./routes/reports.js');
