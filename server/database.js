379e76e7d59d42e3b24321c09d78517a1790ecd1
Daniel J Ley <danieljley@Admins-MacBook-Pro.local>
2017-01-18 11:31:07 -0500

    Add database logic using Mongoose.

--- server/database.js
+++ server/database.js
@@ -1,7 +1,41 @@
 'use strict';
 
-let mongoose = require('mongoose');
-let Report = require('./models/Report.js');
+const mongoose = require('mongoose');
+const Report = require('./models/Report.js');
+const https = require('https');
+
+let reports = [];
+
+var jsonUrl = 'https://s3.amazonaws.com/verizoniot/vzthingspace/staging/v2/2017-01-05/thingspacereport.json';
+https.get(jsonUrl, (res) => {
+  const statusCode = res.statusCode;
+  const contentType = res.headers['content-type'];
+
+  let error;
+  if (statusCode !== 200) {
+    error = new Error(`Request Failed.\n` +
+                      `Status Code: ${statusCode}`);
+  } else if (!/^application\/json/.test(contentType)) {
+    error = new Error(`Invalid content-type.\n` +
+                      `Expected application/json but received ${contentType}`);
+  }
+  if (error) {
+    console.log(error.message);
+    // consume response data to free up memory
+    res.resume();
+    return;
+  }
+
+  res.setEncoding('utf8');
+  let rawData = '';
+  res.on('data', (chunk) => rawData += chunk);
+  res.on('end', () => {
+      let parsedData = JSON.parse(rawData);
+      reports = [parsedData];
+  });
+}).on('error', (e) => {
+  console.log(`Got error: ${e.message}`);
+  });
 
 mongoose.connect('mongodb://localhost/test', () => {
   console.log("connected");
